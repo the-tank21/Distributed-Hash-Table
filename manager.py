863db3f308f8c71ac65ae5fc3395191b7c37f810
the-tank21 <144883652+the-tank21@users.noreply.github.com>
2024-02-11 20:05:54 -0700

    Added port_number check and psuedocode

--- manager.py
+++ manager.py
@@ -5,10 +5,19 @@ import socket
 # Create and bind socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 host = socket.gethostname()
-port_number = int(input("Enter port number: "))
+port_number = int(input("Enter port number between 14000 and 14500: "))
+while (port_number < 14000 or port_nummber > 14500):
+    port_number = int(input("Enter port number between 14000 and 14500: "))
 s.bind((host, port_number))
 
 # Run server
 # Listen for command
 # Identify type of command
 # Execute command
+
+
+# Define register function
+
+# Define setep_dht function
+
+# Define dht_complete function
