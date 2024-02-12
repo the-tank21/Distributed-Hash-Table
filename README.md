d60bedb8ae0dbaae26b7727d892c68dd205849d5
the-tank21 <144883652+the-tank21@users.noreply.github.com>
2024-02-12 11:44:25 -0700

    Started peer.py and updated README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CSE434_Socket_Project
 
+Author: Ethan Hodge
+Course: CSE 434 Spring 24
+Desc: Implementation of a distributed hash table over a local area network. 
--- peer.py
+++ peer.py
@@ -1,1 +1,14 @@
 # Peer process for socket project
+import socket
+
+# Define variables
+peer_name = ""
+m_port = 0
+p_pport = 0
+peer_state = "Free"
+
+# Define peer functions
+
+
+
+# Define main loop and response to each function
