84c8b207ef911c703a922335928e477a5633fcce
the-tank21 <144883652+the-tank21@users.noreply.github.com>
2024-02-11 19:59:09 -0700

    Added socket creation and binding plus psuedocode for future

--- manager.py
+++ manager.py
@@ -2,7 +2,13 @@
 
 import socket
 
-port_number = int(input())
-hostname = socket.gethostname()
-host_socket = socket(hostname, port_number)
+# Create and bind socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+host = socket.gethostname()
+port_number = int(input("Enter port number: "))
+s.bind((host, port_number))
 
+# Run server
+# Listen for command
+# Identify type of command
+# Execute command
