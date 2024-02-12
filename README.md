33bfcc147ae49417ff3ee1bc3353ba1cceecd652
the-tank21 <144883652+the-tank21@users.noreply.github.com>
2024-02-12 15:14:43 -0700

    Progress on peer.py

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # CSE434_Socket_Project
 
-Author: Ethan Hodge
-Course: CSE 434 Spring 24
-Desc: Implementation of a distributed hash table over a local area network. 
+Author: Ethan Hodge \n
+Course: CSE 434 Spring 24 \n
+Desc: Implementation of a distributed hash table over a local area network. \n
--- peer.py
+++ peer.py
@@ -4,11 +4,39 @@ import socket
 # Define variables
 peer_name = ""
 m_port = 0
-p_pport = 0
+p_port = 0
 peer_state = "Free"
+id = 0 # Identifier for each peer in the DHT ring
+
+# Manager info
+manager_addr = ""
+manager_port = 0
 
 # Define peer functions
 
 
 
 # Define main loop and response to each function
+def main():
+    # Establish name and port numbers
+    peer_name = input("Enter peer name: ")
+    m_port = int(input("Enter peer-manager port: "))
+    while (m_port < 14000 or m_port > 14500):
+        m_port = int(input("Invalid port number. Please enter a valid port number: "))
+    p_port = int(input("Enter peer-peer port: "))
+    while (p_port < 14000 or p_port > 14500):
+        p_port = int(input("Invalid port number. Please enter a valid port number: "))
+    manager_addr = input("Enter manager address: ")
+    manager_port = int(input("Enter manager port: "))
+    while (manager_port < 14000 or manager_port > 14500):
+        manager_port = int(input("Invalid port number. Please enter a valid port number: "))
+    # Create socket for peer-manager communication
+    m_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    m_socket.bind((socket.gethostname(), m_port))
+
+    # Create socket for peer-peer communication
+    p_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    p_socket.bind((socket.gethostname(), p_port))
+
+    
+    
